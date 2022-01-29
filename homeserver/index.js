90bd0cd468d2fe62082a870f9f92a88f336b620f
Praveenkollipara <praveenkollipara19@gmail.com>
2022-01-29 17:40:55 +0530

    db connceted to P

--- homeserver/index.js
+++ homeserver/index.js
@@ -15,7 +15,8 @@ app.use(cors(corsOptions))
 app.use(express.json());
 
 let mc = require("mongodb").MongoClient;
-const dburl = "mongodb+srv://sushma:sushma_7@cluster0.shzv7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const dburl1 = "mongodb+srv://bobby:19062002pk@cluster0.ucbh4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const dburl="mongodb+srv://sushma:sushma_7@cluster0.shzv7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 let dbObj;
 let collectionObj;
 mc.connect(dburl,{useNewUrlParser:true,useUnifiedTopology:true})
